27d4d46042ba063daf8e2fc58352b18cc668784e
minimal-ui-kit <minimal.ui.kit@gmail.com>
2022-08-13 13:41:41 +0700

    Fix router

--- src/routes.js
+++ src/routes.js
@@ -26,16 +26,25 @@ export default function Router() {
       ],
     },
     {
+      path: 'login',
+      element: <Login />,
+    },
+    {
+      path: 'register',
+      element: <Register />,
+    },
+    {
       path: '/',
       element: <LogoOnlyLayout />,
       children: [
         { path: '/', element: <Navigate to="/dashboard/app" /> },
-        { path: 'login', element: <Login /> },
-        { path: 'register', element: <Register /> },
         { path: '404', element: <NotFound /> },
         { path: '*', element: <Navigate to="/404" /> },
       ],
     },
-    { path: '*', element: <Navigate to="/404" replace /> },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
   ]);
 }
