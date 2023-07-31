1ff7fd973c2ddfaacfd1ca998c5cb25c9c67fd06
Dimitrije Gladovic <rdomiran@gmail.com>
2023-07-31 17:28:22 +0200

    save button added on receipt scan

--- src/components/recit-extractor/RecitExtractor.js
+++ src/components/recit-extractor/RecitExtractor.js
@@ -12,7 +12,30 @@ export default function RecitExtractor(){
     const [message, setMessage] = useState([]);
     const [isFetching, setFetching] = useState([false]);
 
+    const SaveButton = {
+        width:'48%', 
+        height:'50px',
+        border: 'solid rgb(0,175,120) 2px',
+        borderRadius: '10px',
+        background: 'rgb(0,175,120)',
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: '14px'
+    };
+
+    const CancelButton = {
+        width:'48%', 
+        height:'50px',
+        border: 'solid rgb(0,175,120) 2px',
+        borderRadius: '10px',
+        background: 'white',
+        color: 'rgb(0,175,120)',
+        fontWeight: 'bold',
+        fontSize: '14px'
+    };
+
     const urlbase = 'http://localhost:4800/scan/=?';
+    const mainUrlBase = 'http://localhost:4800/';
 
     // const arrayDisplay = [];
 
@@ -29,13 +52,30 @@ export default function RecitExtractor(){
         );
     };
 
+    const saveReceipt = (message) =>{
+        console.log(message);
+        axios.post('http://localhost:4800/scan/save',message.data).then(
+            (e) => {
+                console.log('Saved successfully!');
+                setFetching(false);
+            }    
+        );
+    }
+
     return (
         <div style={{width:'100%',height:'100%'}}>
             {
                 isFetching !== true ? <ReadQr sendQrData={getData}/> : ''
             }
             {
-                isFetching === true ? <Receipt amo={message}/> : ''
+                isFetching === true ? 
+                <>
+                    <Receipt amo={message}/>
+                    <div style={{margin: 'auto', marginTop: '10px', width:'397px', display: 'flex', justifyContent: 'space-between'}}>
+                        <button style={SaveButton} onClick={() => saveReceipt(message)}>SAVE</button>
+                        <button style={CancelButton} onClick={ ()=>setFetching(false)}>CANCEL</button>
+                    </div>
+                </> : ''
             }
         </div>
     )
