49f99d282f4cf7d84889ba4216f0960f787b7a08
Dimitrije Gladovic <rdomiran@gmail.com>
2023-08-10 17:43:46 +0200

    ReceiptHeader date Format update

--- src/components/receipt-header/ReceiptHeader.js
+++ src/components/receipt-header/ReceiptHeader.js
@@ -4,6 +4,18 @@ import SvgColor from "../svg-color";
 
 const ReceiptHeader = ({receipt}) =>{
 
+    function formatDate(date) {
+        date = new Date(date);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        const seconds = String(date.getSeconds()).padStart(2, '0');
+      
+        return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+      }
+
 
 return (
 <>
@@ -13,7 +25,7 @@ return (
         <SvgColor src={'ic_store.svg'} sx={{ width: 1, height: 1 }}/>
     </div>
     <h3 style={{margin: 'auto',display:'block',marginTop:'4px'}}>{receipt.name}</h3>
-    <div style={{margin: 'auto', marginBottom:'10px', fontSize: '12px'}}>{receipt.timeDate[0]}</div>            
+    <div style={{margin: 'auto', marginBottom:'10px', fontSize: '12px'}}>{formatDate(receipt.timeDate)}</div>            
 </div>
 </>)
 };
