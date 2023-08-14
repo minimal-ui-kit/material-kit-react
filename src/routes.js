f571f948894f48f676db3cad42dd58d1203ed66f
Dimitrije Gladovic <dimitrije.gladovic@asseco-see.rs>
2023-08-14 16:04:27 +0200

    authentication added through context api

--- src/routes.js
+++ src/routes.js
@@ -36,7 +36,7 @@ export default function Router() {
     {
       element: <SimpleLayout />,
       children: [
-        { element: <Navigate to="/dashboard/app" />, index: true },
+        { element: <Navigate to="/login" />, index: true },
         { path: '404', element: <Page404 /> },
         { path: '*', element: <Navigate to="/404" /> },
       ],
