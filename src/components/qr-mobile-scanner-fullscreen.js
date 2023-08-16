7ec922e149cfb816d229921c7e0e894771acf6a6
Dimitrije Gladovic <dimitrije.gladovic@asseco-see.rs>
2023-08-16 17:05:18 +0200

    QR Code Scanner through full screen dialog

---
+++ src/components/qr-mobile-scanner-fullscreen.js
@@ -0,0 +1,41 @@
+/* eslint-disable jsx-a11y/media-has-caption */
+/* eslint-disable arrow-body-style */
+
+import { useState, useRef, useEffect } from 'react';
+import QrScanner from 'qr-scanner';
+import { Dialog, DialogContent } from '@mui/material'
+import ReadQrMobile from './qr-mobile-scanner';
+
+const ReadQrMobileFullscreen = ({sendQrData}) => {
+
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const sendQrDataNew = (message)=>{
+        handleClose();
+        sendQrData(message);
+    }
+
+    return (
+        <div style={{ margin: "auto", border: 'solid red 2px' }}>
+            <Dialog open={open} onClose={handleClose}>
+                <ReadQrMobile sendQrData={sendQrDataNew}/>
+            </Dialog>
+            <button type="button"
+                style={{ margin: 'auto', display: 'block', backgroundColor: 'rgb(0,140,120)', color: 'white', border: 'solid rgb(0,175,120) 5px', borderRadius: '50%', width: '150px', height: '150px' }}
+                onClick={handleOpen}>
+                Open Dialog
+            </button>
+        </div>
+
+    );
+};
+
+export default ReadQrMobileFullscreen;
--- src/components/qr-mobile-scanner.js
+++ src/components/qr-mobile-scanner.js
@@ -1,53 +1,38 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable arrow-body-style */
 
-import {useState, useRef} from 'react';
+import { useState, useRef, useEffect } from 'react';
 import QrScanner from 'qr-scanner';
+import { Dialog, DialogContent } from '@mui/material'
 
-const ReadQrMobile = ({sendQrData}) =>{
-    
-    const videoRef = useRef(null);
+const ReadQrMobile = ({sendQrData}) => {
+
+    const videoRef = useRef();
+
     const [result, setResult] = useState('');
 
-    const [file,setFile] = useState(null);
-    const [data,setData] = useState(null);
+    const [data, setData] = useState(null);
 
-    const fileRef = useRef();
-
-    const startScan = async () => {
+    useEffect(()=>{
         const qrScanner = new QrScanner(videoRef.current,
-            result => console.log(result),
-          {
-            highlightScanRegion:"true",
-            highlightCodeOutline: "true"
-          });
-          qrScanner.start();
-          const result = await QrScanner.scanImage(videoRef.current);
-          setData(result);
-          sendQrData(result);
-        };
+            result => {
+                setData(result);
+                sendQrData(result);
+                qrScanner.stop();
+            },
+            {
+                highlightScanRegion: "true",
+                highlightCodeOutline: "true",
+                returnDetailedScanResult: true
+            });
+        qrScanner.start();
+    });
 
-    // const handleClick = () =>{
-    //     fileRef.current.click();
-    // }
-
-    // const handleChange = async (e) =>{
-    //     const file = e.target.files[0];
-    //     setFile(file);
-    //     const result = await QrScanner.scanImage(file);
-    //     setData(result);
-    //     sendQrData(result);
-    // }
-
-    return(
-        <div style={{ margin: "auto", width:'150px',height:'150px',marginTop:'250px'}}>
-            <video ref={videoRef} style={{ margin: "auto", width: '500px', maxWidth: '500px' }}/>
-            <button type="button"
-            style={{margin: 'auto', backgroundColor:'rgb(0,140,120)', color:'white', border:'solid rgb(0,175,120) 5px',borderRadius:'50%',width:'150px',height:'150px'}}
-            onClick={startScan}>
-                Scan Mobile
-            </button>
-
+    return (
+        <div style={{ margin: "auto", border: 'solid red 2px' }}>
+            <div style={{ width: '400px', height: '400px', overflow: 'hidden' }}>
+                <video id="video" ref={videoRef} playsInline style={{ width: '100%', height: '100%', objectFit: 'cover', border: 'solid black 2px' }} />
+            </div>
         </div>
 
     );
