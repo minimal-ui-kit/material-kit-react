8b2574bf8c1ad02b04d0696a6214bd3033f150d3
Om Buddhadev <buddhadevom@gmail.com>
2024-03-04 16:05:44 +0530

    results

--- src/components/result/results.jsx
+++ src/components/result/results.jsx
@@ -11,19 +11,39 @@ const Results = (props) => {
             <h2 style={{textAlign: "center"}}>{props.props.MatchName}</h2>
             <h2 style={{textAlign: "center"}}>{props.props.GroupStage}</h2>
         </Box>
-        <Box sx={{display:"flex", flexDirection:"column"}}>
-            <Typography textAlign={"center"}>
+        <Box sx={{display:"flex", flexDirection:"column", } }>
+            <Typography fontSize={"20px"} textAlign={"center"} mb={3}>
                 {props.props.Date}
             </Typography>
-            <Box sx={{display: "flex", flexDirection: "row", justifyContent: "space-between", marginX: "35px"}}>
+            <Box sx={{display: "flex", flexDirection: "row", justifyContent: "space-between", marginX: "15px"}}>
                 <Box>
                 <img width="100px"alt="logo" src={props.props.ClgImg1}>
                 </img>
                 <Typography textAlign={"center"}>{props.props.ClgName1}</Typography>
                 </Box>
+                
+                {props.props.Type==="football" && <Box><Typography fontSize={"50px"}>{props.props.Score}</Typography></Box>}
+                {props.props.Type==="cricket" && 
+                
+                    <Box sx={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                        <Box>
+                        <Typography fontSize={"40px"} mr={4}>{props.props.Score1}</Typography>
+                        <Typography textAlign={"center"} fontSize={"25px"} mr={4}>{props.props.Over1}</Typography>
+                        </Box>
+                        <Box>
+                        <Typography fontSize={"40px"}>{props.props.Score2}</Typography> <Typography textAlign={"center"} fontSize={"25px"} mr={4}>{props.props.Over1}</Typography></Box>
+                    </Box>
+              
+                
+                }
+                {props.props.Type==="athlete" && 
                 <Box>
-                <Typography fontSize={"50px"}>{props.props.Score}</Typography>
+                    <Typography fontSize={"25px"} mr={4}>1 - {props.props.Player1}</Typography>
+                    <Typography fontSize={"25px"} mr={4}>2 - {props.props.Player2}</Typography>
+                    <Typography fontSize={"25px"} mr={4}>3 - {props.props.Player3}</Typography>
                 </Box>
+                }
+                
                 <Box>
                 <img width="100px" alt="logo" src={props.props.ClgImg2}>
                 </img>
