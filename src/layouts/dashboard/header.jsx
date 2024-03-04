cce65dc64ca37c952bbf39a49ce1ec0b0c9be1d5
Om Buddhadev <buddhadevom@gmail.com>
2024-03-04 17:14:54 +0530

    head

--- src/layouts/dashboard/header.jsx
+++ src/layouts/dashboard/header.jsx
@@ -16,6 +16,8 @@ import { NAV, HEADER } from './config-layout';
 import AccountPopover from './common/account-popover';
 import LanguagePopover from './common/language-popover';
 import NotificationsPopover from './common/notifications-popover';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 // ----------------------------------------------------------------------
 
@@ -32,20 +34,41 @@ export default function Header({ onOpenNav }) {
         </IconButton>
       )}
 
-      <Searchbar />
+      
 
       <Box sx={{ flexGrow: 1 }} />
 
-      <Stack direction="row" alignItems="center" spacing={1}>
-        <LanguagePopover />
-        <NotificationsPopover />
-        <AccountPopover />
-      </Stack>
+    
     </>
   );
 
   return (
-    <></>
+    <AppBar
+      sx={{
+        boxShadow: 'none',
+        height: HEADER.H_MOBILE,
+        zIndex: theme.zIndex.appBar + 1,
+        ...bgBlur({
+          color: theme.palette.background.default,
+        }),
+        transition: theme.transitions.create(['height'], {
+          duration: theme.transitions.duration.shorter,
+        }),
+        ...(lgUp && {
+          width: `calc(100% - ${NAV.WIDTH + 1}px)`,
+          height: HEADER.H_DESKTOP,
+        }),
+      }}
+    >
+      <Toolbar
+        sx={{
+          height: 1,
+          px: { lg: 5 },
+        }}
+      >
+        {renderContent}
+      </Toolbar>
+    </AppBar>
   );
 }
 
