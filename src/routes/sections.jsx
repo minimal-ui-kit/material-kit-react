e0374ebf149189ed9dc33986d62cfbaec2b46fd1
Om Buddhadev <buddhadevom@gmail.com>
2024-03-04 16:49:05 +0530

    ch

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -13,7 +13,8 @@ export const UserPage = lazy(() => import('src/pages/user'));
 export const LoginPage = lazy(() => import('src/pages/login'));
 export const ProductsPage = lazy(() => import('src/pages/products'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
-export const SecretPage = lazy(()=> import('src/pages/asmitaasmita'))
+export const SecretPage = lazy(()=> import('src/pages/asmitaasmita'));
+export const AddUserPage = lazy(()=> import('src/pages/add-user'));
 
 // ----------------------------------------------------------------------
 
@@ -54,12 +55,17 @@ export default function Router() {
         },
         { path: 'blog', element: <BlogPage /> },
         { path: 'asmitaasmita', element: <SecretPage /> },
+        {
+          path: 'adduser',
+          element: <AddUserPage />,
+        },
       ],
     },
     {
       path: 'login',
       element: <LoginPage />,
     },
+    
     {
       path: '404',
       element: <Page404 />,
---
+++ src/sections/head/index.js
@@ -0,0 +1,1 @@
+export { default as HeadView } from './head-view';
