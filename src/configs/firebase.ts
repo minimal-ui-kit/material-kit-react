f02dd63f48713ca2dd1e3525800886beac85d74d
natehubbit007 <Hubert.Mbitor@juduh.com>
2025-01-24 14:57:49 +0000

    ref: renamed envs

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-import type {
-  User} from 'firebase/auth';
+import type { User } from 'firebase/auth';
 
 import { initializeApp } from 'firebase/app';
 import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
@@ -21,13 +20,13 @@ import type { ApiRoute } from '../constants/fxns';
 const USE_EMULATORS = false;
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.me,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 console.log(firebaseConfig);
