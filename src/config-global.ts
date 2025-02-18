7d879d2ccb2984fc6d3e77e8693e13e48d97e251
Elise Oh <elise.ohh1506@gmail.com>
2025-02-18 11:04:27 +0800

    new branch

--- src/config-global.ts
+++ src/config-global.ts
@@ -10,6 +10,6 @@ export type ConfigValue = {
 // ----------------------------------------------------------------------
 
 export const CONFIG: ConfigValue = {
-  appName: 'Minimal UI',
+  appName: 'wyd',
   appVersion: packageJson.version,
 };
