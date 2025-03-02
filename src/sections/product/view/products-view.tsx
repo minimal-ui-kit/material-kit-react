ab1f3a8e3e7313407d47895295fea03fac732c03
coyx2108 <chantelong@u.nus.edu>
2025-03-02 18:26:23 +0800

    edited the filtering of products for activities

--- src/sections/product/view/products-view.tsx
+++ src/sections/product/view/products-view.tsx
@@ -18,48 +18,26 @@ import { CartIcon } from '../product-cart-widget';
 import { ProductFilters } from '../product-filters';
 
 import type { FiltersProps } from '../product-filters';
+import { stat } from 'fs';
 
 
 // ----------------------------------------------------------------------
 
-const GENDER_OPTIONS = [
-  { value: 'men', label: 'Men' },
-  { value: 'women', label: 'Women' },
-  { value: 'kids', label: 'Kids' },
-];
-
-const CATEGORY_OPTIONS = [
-  { value: 'all', label: 'All' },
-  { value: 'shose', label: 'Shose' },
-  { value: 'apparel', label: 'Apparel' },
-  { value: 'accessories', label: 'Accessories' },
-];
-
 const RATING_OPTIONS = ['up4Star', 'up3Star', 'up2Star', 'up1Star'];
 
-const PRICE_OPTIONS = [
-  { value: 'below', label: 'Below $25' },
-  { value: 'between', label: 'Between $25 - $75' },
-  { value: 'above', label: 'Above $75' },
-];
-
-const COLOR_OPTIONS = [
-  '#00AB55',
-  '#000000',
-  '#FFFFFF',
-  '#FFC0CB',
-  '#FF4842',
-  '#1890FF',
-  '#94D82D',
-  '#FFC107',
+const STATUS_OPTIONS = [
+  { value: 'all', label: 'All' },
+  { value: 'upcoming', label: 'Upcoming' },
+  { value: 'completed', label: 'Completed' },
+  { value: 'cancelled', label: 'Cancelled' },
 ];
 
 const defaultFilters = {
-  price: '',
-  gender: [GENDER_OPTIONS[0].value],
-  colors: [COLOR_OPTIONS[4]],
   rating: RATING_OPTIONS[0],
-  category: CATEGORY_OPTIONS[0].value,
+  creditRange: [0, 15],
+  status: STATUS_OPTIONS[0].value,
+  vacanciesRange: [0, 100],
+  // dateRange: [null, null],
 };
 
 export function ProductsView() {
@@ -126,11 +104,11 @@ export function ProductsView() {
             onCloseFilter={handleCloseFilter}
             onResetFilter={() => setFilters(defaultFilters)}
             options={{
-              genders: GENDER_OPTIONS,
-              categories: CATEGORY_OPTIONS,
               ratings: RATING_OPTIONS,
-              price: PRICE_OPTIONS,
-              colors: COLOR_OPTIONS,
+              creditRange: [0, 15],
+              status: STATUS_OPTIONS,
+              vacanciesRange: [0, 100],
+              // dateRange: [null, null],
             }}
           />
 
